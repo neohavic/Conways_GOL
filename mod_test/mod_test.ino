0940979a48f0703756a6adee315b0f2683c7a357
neohavic <30667948+neohavic@users.noreply.github.com>
2025-08-20 22:23:49 -0700

    Rewrote modulo test code
    Modulo test code was rewritten and test via ESP32-S2 emulator to ensure it functions as expected. Still need to update the 8x8 matrix code with it, although it runs perfectly fine as is

--- mod_test/mod_test.ino
+++ mod_test/mod_test.ino
@@ -1,14 +1,22 @@
+/*
+Code to see if the encapsulated modulo function worked correctly...spoiler, it does.
+Verified using the ESP-S3 emulator at: https://wokwi.com/projects/new/esp32-s2
+*/
+
 void setup() {
   // put your setup code here, to run once:
 Serial.begin(9600);
 }
 
 void loop() {
+uint8_t in = 85;
+String test = String(modFixed(in));
   // put your main code here, to run repeatedly:
-Serial.print(8 %8 );
-Serial.print(" ");
-Serial.print(-1 % 8);
-Serial.print(" ");
-Serial.print(((-1 % 8 ) + 8) % 8);
-Serial.print("\n");
+Serial.print("Old way " + String(((in % 64 ) + 64) % 64) + " " + "modFixed " + test + " \r\n");
+delay(250);
+}
+
+uint8_t modFixed(uint8_t in)
+{
+  return (((in % 64) + 64) % 64);
 }
